2c33edee7c7ae82f526ea3115bf5259c8fa396b6
do0134 <do0134@naver.com>
2023-08-02 14:03:35 +0900

    [dev] : MySql 연동

---
+++ wanted_premission/wanted_premission/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        "ENGINE": 'django.db.backends.mysql',
+        'NAME': 'wanted_premission',
+        'USER': 'root',
+        'PASSWORD': 'tonywoo0802!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-uff_vt&z7y7f9c!+@(+c69#$sitry@xr)7o^_u(4rp&osfhtip'
